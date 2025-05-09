38f8890d9c0352b2cb757502600f549735aae4da
raducu.balgiu <raducu.balgiu@vodafone.com>
2025-05-09 20:50:38 +0300

    Add Summary To Roles and Permissions

--- app/api/v1/endpoints/user/permission.py
+++ app/api/v1/endpoints/user/permission.py
@@ -10,26 +10,49 @@ from app.service.user.permission import get_all_permissions, create_new_permissi
 
 router = APIRouter(prefix="/permissions", tags=["Permissions"])
 
-@router.get("/", response_model=PaginatedResponse[PermissionWithRolesResponse])
+@router.get(
+    "/",
+    summary='List All Permissions',
+    response_model=PaginatedResponse[PermissionWithRolesResponse])
 async def get_permissions(db: DBSession, page: int, limit: int):
     return await get_all_permissions(db, page, limit)
 
-@router.post("/", response_model=PermissionResponse, dependencies=[SuperAdminSession])
+@router.post(
+    "/",
+    summary='Create New Permission',
+    response_model=PermissionResponse,
+    dependencies=[SuperAdminSession])
 async def create_permission(db: DBSession, permission_create: PermissionCreate):
     return await create_new_permission(db, permission_create)
 
-@router.put("/{permission_id}", response_model=PermissionResponse, dependencies=[SuperAdminSession])
+@router.put(
+    "/{permission_id}",
+    summary='Update Permission',
+    response_model=PermissionResponse,
+    dependencies=[SuperAdminSession])
 async def update_permission(db: DBSession, permission_id: int, permission_update: PermissionUpdate):
     return await update_permission_by_id(db, permission_id, permission_update)
 
-@router.delete("/{permission_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[SuperAdminSession])
+@router.delete(
+    "/{permission_id}",
+    summary='Delete Permission',
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[SuperAdminSession])
 async def delete_permission(db: DBSession, permission_id: int):
     return await delete_permission_by_id(db, permission_id)
 
-@router.post("/{permission_id}/roles/{role_id}", status_code=status.HTTP_201_CREATED, dependencies=[SuperAdminSession])
+@router.post(
+    "/{permission_id}/roles/{role_id}",
+    summary='Create Role - Permission Relation',
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[SuperAdminSession])
 async def attach_role_permission(db: DBSession, permission_id: int, role_id: int):
     return await attach_role_to_permission(db, permission_id, role_id)
 
-@router.delete("/{permission_id}/roles/{role_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[SuperAdminSession])
+@router.delete(
+    "/{permission_id}/roles/{role_id}",
+    summary='Remove Role - Permission Relation',
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[SuperAdminSession])
 async def detach_role_permission(db: DBSession, permission_id: int, role_id: int):
     return await detach_role_from_permission(db, permission_id, role_id)
--- app/api/v1/endpoints/user/role.py
+++ app/api/v1/endpoints/user/role.py
@@ -8,18 +8,30 @@ from app.service.user.role import get_all_roles, create_new_role, delete_role_by
 
 router = APIRouter(prefix="/roles", tags=["Roles"])
 
-@router.get("/", response_model=PaginatedResponse[RoleResponse])
+@router.get(
+    "/",
+    summary='List All Roles',
+    response_model=PaginatedResponse[RoleResponse])
 async def get_roles(db: DBSession, page: int, limit: int):
     return await get_all_roles(db, page, limit)
 
-@router.post("/", response_model=RoleResponse)
+@router.post(
+    "/",
+    summary='Create New Role',
+    response_model=RoleResponse)
 async def create_role(db: DBSession, role_create: RoleCreate):
     return await create_new_role(db, role_create)
 
-@router.put("/{role_id}", response_model=RoleResponse)
+@router.put(
+    "/{role_id}",
+    summary='Update Role',
+    response_model=RoleResponse)
 async def update_role(db: DBSession, role_id: int, role_update: RoleUpdate):
     return await update_role_by_id(db, role_id, role_update)
 
-@router.delete("/{role_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete(
+    "/{role_id}",
+    summary='Delete Role',
+    status_code=status.HTTP_204_NO_CONTENT)
 async def delete_role(db: DBSession, role_id: int):
     return await delete_role_by_id(db, role_id)
