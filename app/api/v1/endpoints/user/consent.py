c47181c74b61c364d5565b7cd4b06c498bedfb5c
raducu.balgiu <raducu.balgiu@vodafone.com>
2025-05-09 19:49:07 +0300

    Add Summary To Consent

--- app/api/v1/endpoints/user/consent.py
+++ app/api/v1/endpoints/user/consent.py
@@ -6,10 +6,17 @@ from app.service.user.consent import get_consent_by_name, create_new_consent
 
 router = APIRouter(prefix="/consents", tags=["Consents"])
 
-@router.get("/{consent_name}", response_model=ConsentResponse)
+@router.get(
+    "/{consent_name}",
+    summary='List All Consents Filtered By Consent Name',
+    response_model=ConsentResponse)
 async def get_consent(db: DBSession, consent_name: str):
     return await get_consent_by_name(db, consent_name)
 
-@router.post("/", response_model=ConsentResponse, dependencies=[SuperAdminSession])
+@router.post(
+    "/",
+    summary='Create New Consent',
+    response_model=ConsentResponse,
+    dependencies=[SuperAdminSession])
 async def create_consent(db: DBSession, consent_create: ConsentCreate):
     return await create_new_consent(db, consent_create)
